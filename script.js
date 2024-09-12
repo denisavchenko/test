d47f472fca2674585165a3861213d60f25a6281b
denisavchenko <137358901+denisavchenko@users.noreply.github.com>
2024-09-12 16:56:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.menu_button').addEventListener('click', function() {
+        document.querySelector('.slider_menu').classList.toggle('slider_menu_active');
+    });
+});
+
